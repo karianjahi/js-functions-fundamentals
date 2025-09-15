cd10e4a4e9d12c2db969465cf467a41781f04c77
karianjahi <kitgaedesigaidi8@gmail.com>
2025-09-15 09:02:17 +0200

    Create a working function on conditions for getting certain loans

--- source.js
+++ source.js
@@ -38,7 +38,58 @@ let email = "apple.pie@example.com"
 maskEmail(email);
 
 
+/*
+Building a loan qualification checker app
+*/
 
-function loanQualificationChecker() {
+/*
+Step 1
+We are building a loan qualification checker app.
 
+The app will check whether the user is eligible for a duplex, car, and condo loan based on their annual income and credit score. A credit score is a number that represents how good you are at managing borrowed money.
+*/
+
+const minIncomeForDuplex = 60000;
+const minCreditScoreForDuplex = 700;
+const minIncomeForCondo = 45000;
+const minCreditScoreForCondo = 680;
+const minIncomeForCar = 30000;
+const minCreditScoreForCar = 650;
+
+// function getLoanMessage(annualIncome, creditScore) {
+//     // Is the user eligible for duplex loan?
+//     if (annualIncome >= minIncomeForDuplex && creditScore >= minCreditScoreForDuplex) {
+//         return "You qualify for a duplex, condo, and car loan."
+//     }
+    
+//     if(annualIncome >= minIncomeForCondo && creditScore >= minCreditScoreForCondo) {
+//         return "You qualify for a condo and car loan";
+//     }
+// }
+
+
+function getLoanMessage(annualIncome, creditScore) {
+  if(creditScore >= minCreditScoreForDuplex && annualIncome >= minIncomeForDuplex) {
+    return "You qualify for a duplex, condo, and car loan."
+  } else if(annualIncome >= minIncomeForCondo && creditScore >= minCreditScoreForCondo) {
+      return "You qualify for a condo and car loan.";
+    } else if (annualIncome >= minIncomeForCar && creditScore >= minCreditScoreForCar) {
+        return "You qualify for a car loan.";
+    } else if (annualIncome < minIncomeForCar && creditScore < minCreditScoreForCar) {
+        return "You don't qualify for any loans."
+    }
 }
+
+//testing out the function
+let duplexLoanMsg;
+duplexLoanMsg = getLoanMessage(85000, 850);
+console.log(duplexLoanMsg);
+let condoLoanMsg;
+condoLoanMsg = getLoanMessage(65000, 690);
+console.log(condoLoanMsg);
+let carLoanMsg;
+carLoanMsg = getLoanMessage(45000, 660);
+console.log(carLoanMsg);
+let noLoanMsg;
+noLoanMsg = getLoanMessage(25000, 550);
+console.log(noLoanMsg);
